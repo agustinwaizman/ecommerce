6f03dbcbf4202a10916901eb01fc075290590077
Agustin <aguswaizman98@gmail.com>
2023-01-30 17:58:09 -0300

    37

--- apps/products/api/views/general_views.py
+++ apps/products/api/views/general_views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.response import Response
 from apps.base.api import GeneralListApiView
 from apps.products.models import MeasureUnit
@@ -50,3 +51,37 @@ class IndicatorViewSet(viewsets.GenericViewSet):
 
 class CategoryProductViewSet(viewsets.GenericViewSet):
     serializer_class = CategoryProductSerializer
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(state = True)
+
+    def get_object(self):
+        return self.get_serializer().Meta.model.objects.filter(id = self.kwargs['pk'], state = True)
+
+    def list(self, request):
+        data = self.get_queryset()
+        data = self.get_serializer(data, many=True)
+        return Response(data.data)
+
+    def create(self, request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'Categoria registrada correctamente!'}, status=status.HTTP_200_OK)
+        return Response({'message':'', 'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        if self.get_object().exists():
+            serializer = self.serializer_class(instance=self.get_object().get(), data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message':'Categoria actualizada correctamente!'}, status=status.HTTP_200_OK)
+        return Response({'message':'', 'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        if self.get_object().exists():
+            category = self.get_object().get()
+            category.state = False
+            category.save()
+            return Response({'message':'Categoria eliminada correctamente!'}, status=status.HTTP_200_OK)
+        return Response({'message':'No se encontro la categoria'}, status=status.HTTP_400_BAD_REQUEST)
